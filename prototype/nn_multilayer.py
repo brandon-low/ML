beca26690bff6e8e7942dc2dc84ba62e762262b7
brandon low <brandonlow@ymail.com>
2022-11-17 02:53:09 +1100

    clean up

--- prototype/nn_multilayer.py
+++ prototype/nn_multilayer.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
+import pickle
+from keras.datasets import mnist
 import matplotlib.pyplot as plt
+
 
 # tanh activation function
 def tanh(x: np.ndarray) -> np.ndarray:
@@ -65,11 +68,11 @@ def one_hot(Y):
     one_hot_Y[Y,np.arange(Y.size)] = 1 # met un 1 en ligne Y[i] et en colonne i, change l'ordre mais pas le nombre
     return one_hot_Y
 
-def init_params(cfg, n_layers: int):
+def init_params(cfg):
     weights = []
     biases = []
     np.random.seed(1)
-    for i in range(n_layers -1):
+    for i in range(len(cfg) -1):
         #print(nn_cfg[i+1] ,":", nn_cfg[i])
         #w.append(np.random.randn( cfg[i+1], cfg[i])) # dont use this 
          
@@ -77,65 +80,65 @@ def init_params(cfg, n_layers: int):
         weights.append(np.random.normal(size=(cfg[i+1], cfg[i])) * np.sqrt(1./(cfg[i])) )
         biases.append(np.zeros((cfg[i+1], 1)))
         
-        """
         if (i == 0):
-            print("Input Layer W:", w[i].shape)
-        elif (i == (n_layers-2) ):
-            print("Output Layer W:", w[i].shape)
+            print("W:", weights[i].shape,"B:",biases[i].shape, "Input Layer")
+        elif (i == (len(cfg)-2) ):
+            print("W:", weights[i].shape, "B:",biases[i].shape, "Output Layer")
         else:
-            print("Hidden Layer:W", w[i].shape)
-        """
+            print("W:", weights[i].shape, "B:",biases[i].shape, "Hidden Layer")
+            
+    
     return weights, biases
     
-def forwardfeed(x: np.ndarray, weights, biases, n_layer: int):
-    z = []
-    a = []
-    z.append(x)
-    a.append(x)
+def forwardfeed(x: np.ndarray, weights, biases):
+    zetas = []      # zetas
+    acts = []      # activated
+    
+    # set the 1st entry
+    zetas.append(x)
+    acts.append(x)
+    
+    nWeights=len(weights)
+    
     #print("**** FORWARD FEED  return the last activation which is the result ******")
-    for i in range(n_layer - 1):
-            
-        z.append(weights[i].dot(a[i]) + biases[i])
+    for i in range(nWeights):
+        zetas.append(weights[i].dot(acts[i]) + biases[i])
         #print("z[-1]:", z[-1].shape , "i:", i, "z[",(i+1),"]", z[i+1].shape, "z[",i,"]", z[i].shape)
         
-        if (i == (n_layer-2) ):
+        if (i == (nWeights-1) ):
             #print("Forward Output Layer W:", w[i].shape)
-            a.append(softmax(z[i+1]))
+            acts.append(softmax(zetas[i+1]))
         else:
-            a.append(activation(z[i+1]))
+            acts.append(activation(zetas[i+1]))
         
         #print("ForwardFeed i:", i, "z[-1]:", z[-1].shape, "a[-1]:", a[-1].shape)
     
     #print("return at forward a[-1]:", a[-1].shape)
     #print("**** FORWARD FEED FINISH******")
-    return z, a, a[-1]
+    return zetas, acts, acts[-1]
 
-def backward_propagate(w, b, z, a, n_layer, Y):
+def backward_propagate(weights, biases, zetas, acts, Y):
     #print("**** BACK PROP  START ****")
     gradw = []
     gradb = []
     deltas = []
     
     #print("delta:", a[-1].shape, "-", Y_train.shape , "=", (a[-1] - Y_train).shape )
-    
     one_hot_Y = one_hot(Y)
-    #dZ2 = 2*(A2 - one_hot_Y) #10,m
-    deltas.append( 2 * (a[-1] - one_hot_Y)) 
+    deltas.append( 2 * (acts[-1] - one_hot_Y))    #dZ2 = 2*(A2 - one_hot_Y) #10,m
     
-    #deltas.append(a[-1] - Y)  # correct
-
     #print("a[-2]:", a[-2].shape, " a[-2].T:", a[-2].T.shape, "deltas[-1]:", deltas[-1].shape)
     #print("DoLast gradw: " , deltas[-1].dot(a[-2].T).shape)
 
     total_m = deltas[-1].shape[1]
-    #print("deltas to work out gradw & gradb: m:", total_m)
-    mm = 1/total_m
+    #print("0 deltas to work out gradw & gradb: m:", total_m)
+    mm = 1/total_m      ## same number for all
+    
     #gradw.append(np.dot(a[-2].T, deltas[-1])) # wrong size
-    gradw.append(mm* deltas[-1].dot(a[-2].T) ) # delta[out neurons, m]
+    gradw.append(mm * deltas[-1].dot(acts[-2].T) ) # delta[out neurons, m]
     gradb.append(mm * np.sum(deltas[-1])) # 1/mX sum(deltas)
 
     #gradb.append(np.sum(deltas[-1], axis=0, keepdims=True))
-
     #print("Start Loop Range deltas:" , len(deltas), " w:", len(w), "z:", len(z))
     #print("gradw:", gradw[-1].shape, "gradb:", gradb[-1].shape)
     #print("Size a:", len(a) , "a[-1]", a[-1].shape, "a[", (len(a)-1), "]:", a[len(a)-1].shape,
@@ -143,8 +146,8 @@ def backward_propagate(w, b, z, a, n_layer, Y):
 
     #print("***** BUILD Rest Of Deltas , gradw, gradb ******")
     #for i in range(nn_cfg_n_layers - 1, 0, -1):     # loop is wrong should stop at 1
-    
-    for i in range(n_layer - 1, 1, -1):     # loop is right 
+    nWeights=len(weights)
+    for i in range(nWeights, 1, -1):     # loop is right 
         #print("DO:", i);
         ## should use w [i-1]
         #print("z[",(i-1), "]", z[i-1].shape, "w[",i,"]", w[i-1].shape)
@@ -153,23 +156,20 @@ def backward_propagate(w, b, z, a, n_layer, Y):
         #print("deltas[-1]:", deltas[-1].shape)
         #print("dZ=", w[i-1].T.dot(deltas[-1]).shape)
         #dZ1 = W2.T.dot(dZ2)*derivative_ReLU(Z1) # 10, m
-        deltas.append( w[i-1].T.dot(deltas[-1]) * activation_deriv(z[i-1]))
+        deltas.append( weights[i-1].T.dot(deltas[-1]) * activation_deriv(zetas[i-1]))
     
         #print("deltas[-1]:", deltas[-1].shape, "<==use ) a[", (i-2), "]:", 
         #      a[i-2].shape, " (next is wrong shape a[", (i-1),"]:", a[i-1].shape)
         #dW1 = 1/m * (dZ1.dot(X.T)) #10, 784
     
-        total_m = deltas[-1].shape[1]   
-        #print("deltas to work out gradw & gradb: m:", total_m)
-        mm = 1/total_m
         #print("Next gradw:", deltas[-1].dot(a[i-2].T).shape )
-        gradw.append(mm*deltas[-1].dot(a[i-2].T));
+        gradw.append( mm *deltas[-1].dot(acts[i-2].T)); # mm is correct
         # this is the wrong shape should use i-2
         #gradw.append(np.dot(a[i - 1].T, deltas[-1]))
     
         #gradb.append(np.sum(deltas[-1], axis=0, keepdims=True)) # this is good
         #print("deltas to work out gradb: m:", deltas[-1].shape[1])
-        gradb.append( mm * np.sum(deltas[-1])) # 1/mX sum(deltas)
+        gradb.append( mm * np.sum(deltas[-1])) # mm is correct - 1/mX sum(deltas)
         
     gradw.reverse()
     gradb.reverse()
@@ -178,19 +178,69 @@ def backward_propagate(w, b, z, a, n_layer, Y):
     
     return deltas, gradw, gradb
 
-def update_params(weights, biases, gradw, gradb, n_layer, alpha):
+def update_params(weights, biases, gradw, gradb, alpha):
+    
     #print("******** UPDATE weights and biases *******")
-    for i in range(n_layer -1):
+    """
+    for i in range(len(weights)):
         #print("w[", i, "]", w[i].shape, "gradw:", gradw[i].shape)
         weights[i] -= alpha*gradw[i]
         #print("b[", i, "]", b[i].shape, "gradb:", gradb[i].shape, "gradb:", gradb[i] )
         #b2 -= alpha * np.reshape(db2, (10,1))
         biases[i] -= alpha*gradb[i]
     #print("******** FINISH Update weight biases*****")
+    """
+    ### same code as above
+    for w, b, dW, dB in zip(weights, biases, gradw, gradb):
+        w -= alpha*dW
+        b -= alpha*dB
+        
     return weights, biases
     
+  
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    return np.sum(predictions == Y)/Y.size
+  
+def gradient_descent(cfg, X, Y, alpha, iterations):
+    weights, biases = init_params(cfg)
     
-def read_test_data_csv():
+    for i in range(iterations):
+        zetas, acts, A2 = forwardfeed(X, weights, biases)
+        _, gradw, gradb = backward_propagate(weights, biases, zetas, acts, Y)
+        weights, biases = update_params(weights, biases, gradw, gradb,  alpha)
+        
+        if (i+1) % (iterations/10) == 0:
+        #if i % 10 == 0:
+            print(f"Iteration: {i+1} / {iterations}")
+            prediction = get_predictions(A2)
+            print(f'{get_accuracy(prediction, Y):.3%}')
+    
+    return weights, biases
+
+def make_predictions(X, weights, biases):
+    _,_, A = forwardfeed(X, weights, biases)
+    #_, _, _, A2 = forward_propagation(X, W1, b1, W2, b2)
+    predictions = get_predictions(A)
+    return predictions
+
+def show_prediction(index,X, Y, weights, biases, img_width=28, img_height=28, img_scale_factor=255):
+    vect_X = X[:, index,None]
+    prediction = make_predictions(vect_X, weights, biases)
+    label = Y[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+
+    current_image = vect_X.reshape((img_width, img_height)) * img_scale_factor
+
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+
+def read_data_from_zip():
     print("Neural Net Work from train.csv.zip")
     SCALE_FACTOR = 255
     WIDTH = 28  #X_train.shape[1]
@@ -207,8 +257,13 @@ def read_test_data_csv():
     #print(data)
     print("Data Shape:" , data.shape, " m:" ,m , " n:", n)
 
-    #np.random.shuffle(data) 
+    data_test = data[0:1000].T
+    Y_test = data_test[0]
+    X_test = data_test[1:n]
+    X_test = X_test / SCALE_FACTOR
 
+    #np.random.shuffle(data) 
+    
     data_train = data[1000:m].T
     Y_train = data_train[0]
     X_train = data_train[1:n]
@@ -217,66 +272,29 @@ def read_test_data_csv():
     print("X_Train shape:", X_train.shape )
     print("Y_Train shape:", Y_train.shape , " shape[0]:", Y_train.shape[0])
     
-    return X_train, Y_train
-  
-def get_predictions(A2):
-    return np.argmax(A2, 0)
+    return X_train, Y_train, X_test, Y_test, WIDTH, HEIGHT, SCALE_FACTOR
 
-def get_accuracy(predictions, Y):
-    return np.sum(predictions == Y)/Y.size
-  
-def gradient_descent(cfg, n_layer, X, Y, alpha, iterations):
-    #size , m = X.shape
 
-    w, b = init_params(cfg, n_layer)
+def read_data_mnist():
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+    SCALE_FACTOR = 255 # TRES IMPORTANT SINON OVERFLOW SUR EXP
+    WIDTH = X_train.shape[1]
+    HEIGHT = X_train.shape[2]
+
+
+    X_train = X_train.reshape(X_train.shape[0],WIDTH*HEIGHT).T / SCALE_FACTOR
+    X_test = X_test.reshape(X_test.shape[0],WIDTH*HEIGHT).T  / SCALE_FACTOR
     
-    for i in range(iterations):
-        z,a, activatedNeuron = forwardfeed(X, w, b, n_layer)
-        _, gradw, gradb = backward_propagate(w, b, z, a, n_layer, Y)
-        w, b = update_params(w, b, gradw, gradb, n_layer, alpha)
-        
-        #Z1, A1, Z2, A2 = forward_propagation(X, W1, b1, W2, b2)
-        #dW1, db1, dW2, db2 = backward_propagation(X, Y, A1, A2, W2, Z1, m)
-
-        #W1, b1, W2, b2 = update_params(alpha, W1, b1, W2, b2, dW1, db1, dW2, db2)
-
-        #if ((iterations/10) > 0) and ((i+1) % (iterations/10) == 0):
-        if i % 10 == 0:
-            print(f"Iteration: {i} / {iterations}")
-            prediction = get_predictions(activatedNeuron)
-            print(f'{get_accuracy(prediction, Y):.3%}')
-    
-    #return W1, b1, W2, b2
-    return w, b
-
-def make_predictions(X, w, b, n_layer):
-    _,_, A = forwardfeed(X, w, b, n_layer)
-    #_, _, _, A2 = forward_propagation(X, W1, b1, W2, b2)
-    predictions = get_predictions(A)
-    return predictions
-
-def show_prediction(index,X, Y, w, b, n_layer, img_width, img_height, img_scale_factor):
-    vect_X = X[:, index,None]
-    prediction = make_predictions(vect_X, w, b, n_layer)
-    label = Y[index]
-    print("Prediction: ", prediction)
-    print("Label: ", label)
-
-    current_image = vect_X.reshape((img_width, img_height)) * img_scale_factor
-
-    plt.gray()
-    plt.imshow(current_image, interpolation='nearest')
-    plt.show()
-
+    return X_train, Y_train, X_test, Y_test, WIDTH, HEIGHT, SCALE_FACTOR
 
 ##########MAIN ###########
 
-cfg = [784, 10, 10, 10]
-n_layer = len(cfg)
+cfg = [784, 10, 10]
 alpha = 0.15
 
 
-X, Y = read_test_data_csv()
+X_train, Y_train, X_test, Y_test , WIDTH, HEIGHT, SCALE_FACTOR = read_data_mnist()
+#X_train, Y_train, X_test, Y_test, WIDTH, HEIGHT, SCALE_FACTOR = read_data_from_zip()
 
 #w, b = init_params(cfg, n_layer)
 #z,a, _ = forwardfeed(X, w, b, n_layer)
@@ -284,6 +302,23 @@ X, Y = read_test_data_csv()
 #w, b = update_params(w, b, gradw, gradb, n_layer, alpha)
 
 print("Start gradient descent")
-w, b = gradient_descent(cfg, n_layer, X, Y, alpha, 1000)
+weights, biases = gradient_descent(cfg, X_train, Y_train, alpha, 200)
 print("gradient descent END")
+
+""""
+with open("trained_params.pkl","wb") as dump_file:
+    pickle.dump((weights, biases),dump_file)
+
+with open("trained_params.pkl","rb") as dump_file:
+    weights, biases = pickle.load(dump_file)
+"""
+
+show_prediction(0,X_test, Y_test, weights, biases, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(1,X_test, Y_test, weights, biases, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(2,X_test, Y_test, weights, biases, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(100,X_test, Y_test, weights, biases, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(200,X_test, Y_test, weights, biases, WIDTH, HEIGHT, SCALE_FACTOR)
+
+
+
 
--- prototype/nn_two_layers.py
+++ prototype/nn_two_layers.py
@@ -16,11 +16,25 @@ def softmax(Z):
     return exp / exp.sum(axis=0)
 
 def init_params(size):
+    np.random.seed(1)
     W1 = np.random.rand(10,size) - 0.5
     b1 = np.random.rand(10,1) - 0.5
     W2 = np.random.rand(10,10) - 0.5
     b2 = np.random.rand(10,1) - 0.5
     return W1,b1,W2,b2
+
+def init_params_performance(size):
+    np.random.seed(1)
+    W1 = np.random.normal(size=(10, size)) * np.sqrt(1./(size))
+    W2 = np.random.normal(size=(10, 10)) * np.sqrt(1./20)
+
+    #b1 = np.random.normal(size=(10, 1)) * np.sqrt(1./10)
+    #b2 = np.random.normal(size=(10, 1)) * np.sqrt(1./(size))
+    ## definately better with the following
+    b1 = np.zeros((10, 1))
+    b2 = np.zeros((10, 1))
+
+    return W1, b1, W2, b2
 
 def forward_propagation(X,W1,b1,W2,b2):
     Z1 = W1.dot(X) + b1 #10, m
@@ -63,14 +77,14 @@ def get_accuracy(predictions, Y):
 def gradient_descent(X, Y, alpha, iterations):
     size , m = X.shape
 
-    W1, b1, W2, b2 = init_params(size)
+    W1, b1, W2, b2 = init_params_performance(size)
     for i in range(iterations):
         Z1, A1, Z2, A2 = forward_propagation(X, W1, b1, W2, b2)
         dW1, db1, dW2, db2 = backward_propagation(X, Y, A1, A2, W2, Z1, m)
 
         W1, b1, W2, b2 = update_params(alpha, W1, b1, W2, b2, dW1, db1, dW2, db2)
 
-        if (i+1) % int(iterations/10) == 0:
+        if (i+1) % (iterations/10) == 0:
             print(f"Iteration: {i+1} / {iterations}")
             prediction = get_predictions(A2)
             print(f'{get_accuracy(prediction, Y):.3%}')
@@ -81,7 +95,7 @@ def make_predictions(X, W1 ,b1, W2, b2):
     predictions = get_predictions(A2)
     return predictions
 
-def show_prediction(index,X, Y, W1, b1, W2, b2):
+def show_prediction(index,X, Y, W1, b1, W2, b2, WIDTH, HEIGHT, SCALE_FACTOR):
     # None => cree un nouvel axe de dimension 1, cela a pour effet de transposer X[:,index] qui un np.array de dimension 1 (ligne) et qui devient un vecteur (colonne)
     #  ce qui correspond bien a ce qui est demande par make_predictions qui attend une matrice dont les colonnes sont les pixels de l'image, la on donne une seule colonne
     vect_X = X[:, index,None]
@@ -96,30 +110,70 @@ def show_prediction(index,X, Y, W1, b1, W2, b2):
     plt.imshow(current_image, interpolation='nearest')
     plt.show()
 
+def read_data_from_zip():
+    print("Neural Net Work from train.csv.zip")
+    SCALE_FACTOR = 255
+    WIDTH = 28  #X_train.shape[1]
+    HEIGHT = 28     # X_train.shape[2]
+    print("X_Train Width:", WIDTH, " Height:", HEIGHT)
 
+    data = pd.read_csv('../prototype/train.csv.zip',compression='zip')
+    print("Data head:")
+    print(data.head())
+
+    data = np.array(data)
+    m, n = data.shape
+    #print("Read Training Data :")
+    #print(data)
+    print("Data Shape:" , data.shape, " m:" ,m , " n:", n)
+
+    data_test = data[0:1000].T
+    Y_test = data_test[0]
+    X_test = data_test[1:n]
+    X_test = X_test / SCALE_FACTOR
+
+    #np.random.shuffle(data) 
+    
+    data_train = data[1000:m].T
+    Y_train = data_train[0]
+    X_train = data_train[1:n]
+    X_train = X_train / SCALE_FACTOR
+
+    print("X_Train shape:", X_train.shape )
+    print("Y_Train shape:", Y_train.shape , " shape[0]:", Y_train.shape[0])
+    
+    return X_train, Y_train, X_test, Y_test, WIDTH, HEIGHT, SCALE_FACTOR
+
+
+def read_data_mnist():
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+    SCALE_FACTOR = 255 # TRES IMPORTANT SINON OVERFLOW SUR EXP
+    WIDTH = X_train.shape[1]
+    HEIGHT = X_train.shape[2]
+
+
+    X_train = X_train.reshape(X_train.shape[0],WIDTH*HEIGHT).T / SCALE_FACTOR
+    X_test = X_test.reshape(X_test.shape[0],WIDTH*HEIGHT).T  / SCALE_FACTOR
+    
+    return X_train, Y_train, X_test, Y_test, WIDTH, HEIGHT, SCALE_FACTOR
+    
 ############## MAIN ##############
 
-
-
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-SCALE_FACTOR = 255 # TRES IMPORTANT SINON OVERFLOW SUR EXP
-WIDTH = X_train.shape[1]
-HEIGHT = X_train.shape[2]
-X_train = X_train.reshape(X_train.shape[0],WIDTH*HEIGHT).T / SCALE_FACTOR
-X_test = X_test.reshape(X_test.shape[0],WIDTH*HEIGHT).T  / SCALE_FACTOR
+#X_train, Y_train, X_test, Y_test , WIDTH, HEIGHT, SCALE_FACTOR = read_data_mnist()
+X_train, Y_train, X_test, Y_test, WIDTH, HEIGHT, SCALE_FACTOR = read_data_from_zip()
 
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.15, 200)
-
-
 
 with open("trained_params.pkl","wb") as dump_file:
     pickle.dump((W1, b1, W2, b2),dump_file)
 
 with open("trained_params.pkl","rb") as dump_file:
     W1, b1, W2, b2=pickle.load(dump_file)
-show_prediction(0,X_test, Y_test, W1, b1, W2, b2)
-show_prediction(1,X_test, Y_test, W1, b1, W2, b2)
-show_prediction(2,X_test, Y_test, W1, b1, W2, b2)
-show_prediction(100,X_test, Y_test, W1, b1, W2, b2)
-show_prediction(200,X_test, Y_test, W1, b1, W2, b2)
+show_prediction(0,X_test, Y_test, W1, b1, W2, b2, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(1,X_test, Y_test, W1, b1, W2, b2, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(2,X_test, Y_test, W1, b1, W2, b2, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(100,X_test, Y_test, W1, b1, W2, b2, WIDTH, HEIGHT, SCALE_FACTOR)
+show_prediction(200,X_test, Y_test, W1, b1, W2, b2, WIDTH, HEIGHT, SCALE_FACTOR)
+
+
 
